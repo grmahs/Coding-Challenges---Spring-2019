ae277dc909f7221c2295dff710d2760220084aae
Nyasha Zimunhu <nyashaz@seas.upenn.edu>
2019-01-17 11:11:17 -0500

    Add files via upload

---
+++ StringReview.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class StringReview
+{
+    public static void main(String[] args)
+    {
+        Scanner scan=new Scanner(System.in);
+        int t;
+        t=scan.nextInt();
+        while(t>0)
+        {
+            String str=scan.next();
+            for(int i=0;i<str.length();i++)
+            {
+                if(i%2==0)
+                {
+                    System.out.print(str.charAt(i));
+                }   
+            }
+            System.out.print(" ");
+            for(int i=0;i<str.length();i++)
+            {
+                if(i%2==1)
+                {
+                    System.out.print(str.charAt(i));
+                }   
+            }  
+            t=t-1;
+            System.out.println();
+        }   
+    }
+}
