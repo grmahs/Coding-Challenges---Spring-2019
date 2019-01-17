138e668b1625ff38a6c3b2a683799b0020c57dd8
Nyasha Zimunhu <nyashaz@seas.upenn.edu>
2019-01-17 11:37:39 -0500

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class DataTypes{
+	
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+		
+        Scanner scan = new Scanner(System.in);
+
+
+        scan.close();
+    }
+}
